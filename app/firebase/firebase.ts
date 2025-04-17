b253f44999da05afdba288353e25fd1f1f786d75
Nino Berber <95708793+nin0dev@users.noreply.github.com>
2025-04-17 17:43:43 +0200

    fix commonjs error

--- app/firebase/firebase.ts
+++ app/firebase/firebase.ts
@@ -1,9 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-require('dotenv').config();
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import 'dotenv/config';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
